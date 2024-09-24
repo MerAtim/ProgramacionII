b0fe62ad2c24935447846043bc2d6797d9ec4d24
Maria Mercedes Atim <guerra.mercedes@hotmail.com>
2024-09-24 07:34:02 -0300

    Agrego clase 6 de Python

--- Portafolio/Clase 3/Portafolio VueJS/src/App.vue
+++ Portafolio/Clase 3/Portafolio VueJS/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
+import DatosPersonales from './components/DatosPersonales.vue';
 import NavBar from './components/NavBar.vue';
-import DatosPersonales from './components/DatosPersonales.vue'
 </script>
 
 <template>
@@ -9,7 +9,9 @@ import DatosPersonales from './components/DatosPersonales.vue'
         <DatosPersonales />
     </header>
 <main>
-    <section id="educacion"><h2>Educación - Cursos</h2></section>
+    <section id="educacion"><h2>Educación - Cursos</h2>
+    <Educacion />
+    </section>
     <section id="experiencia"><h2>Experiencia</h2></section>
     <section id="proyectos"><h2>Proyectos</h2></section>
     <section id="habilidades"><h2>Habilidades</h2></section>
